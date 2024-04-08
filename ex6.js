25e4acb193c5a2cbcd37046eb877e55aceb3ff44
Lucas-Candinho <lucas_candinho1@estudante.sesisenai.org.br>
2024-04-08 12:31:25 -0300

    finalizado

--- ex6.js
+++ ex6.js
@@ -1,9 +1,9 @@
-var vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+var vogais = ['a', 'e', 'i', 'o', 'u']
 var letra = prompt("Digite uma letra: ");
 var ehVogal = false;
 
 for (let i = 0; i < vogais.length; i++) {
-    if (letra === vogais[i]) {
+    if (letra.toLowerCase() === vogais[i]) {
         i = vogais.length + 1;
         console.log(ehVogal);
         ehVogal = true;
--- ex7.js
+++ ex7.js
@@ -1,2 +1,13 @@
-var sabores = ['Chocolate', 'Morango', 'Creme', 'Manga', 'Melancia', 'Vanilla', 'Céu Azul', 'Brownie', 'Hawaiano'];
+var sabores = ['chocolate', 'morango', 'creme', 'manga', 'melancia', 'vanilla', 'céu azul', 'brownie', 'hawaiano'];
 var valores = [1.50, 2.50, 2.5, 3.20, 3.40, 3.00, 3.60, 4.00, 5.00];
+var escolha = "";
+
+for (let i = 0; i < sabores.length; i++) {
+    console.log(`${sabores[i]}`);
+}
+escolha = prompt("Digite o sabor que você quer: ")
+for (let i = 0; i < sabores.length; i++) {
+    if (escolha.toLowerCase() == sabores[i]) {
+        console.log(`${sabores[i]}` + " | Valor: " + valores[i]) ;
+    }
+}
